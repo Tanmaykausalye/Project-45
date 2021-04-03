aad5a13452ed12618b54d9baf6842bf7790a0812
Tanmaykausalye <73481990+Tanmaykausalye@users.noreply.github.com>
2021-04-03 13:03:16 +0530

    Add files via upload

---
+++ Game.js
@@ -0,0 +1,45 @@
+class Game{
+
+ constructor(){
+
+ }
+
+ getState(){
+
+var getGameState = database.ref('gameState')
+getGameState.on("value",function(data){
+
+gameState = data.val()
+
+})
+}
+
+update(state){
+
+gameState.ref('/').update({
+
+  gameState:state  
+})
+}
+
+async start(){
+
+if(gameState === 0 ){
+
+player = new Player()
+var playerCountref = await database.ref('playerCount').once("value")  
+
+if(playerCountref.exists()){
+
+palyerCount = playerCountref.val()
+player.getCount()
+}
+
+form = new Form()
+form.display()
+
+}
+}
+
+
+}
---
+++ sketch.js
@@ -0,0 +1,20 @@
+var form,game,player,database,playerCount
+var guideline
+
+var gameState = 0
+
+
+
+function setup() {
+  createCanvas(displayWidth-20,displayHeight-30);
+
+ database = firebase.database()
+
+form = new Form()
+
+  }
+
+function draw() {
+  background(255,255,255);  
+  
+}
